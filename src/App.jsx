4f3d2052b117469079411f34153ceac498c10571
Carolina Lunas <carolinalunasfarah@gmail.com>
2025-03-22 14:15:00 -0300

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Home } from "./views/Home";
 import { Route, Routes } from "react-router-dom";
 import SymbolChart from "./components/SymbolChart";
+import Header from "./components/Header";
 
 function App() {
     return (
         <>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/details/:symbol" element={<SymbolChart />} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <header className="d-flex justify-content-center align-content-center">
+      <h1>Finance Track</h1>
+    </header>
+  )
+}
+
+export default Header
