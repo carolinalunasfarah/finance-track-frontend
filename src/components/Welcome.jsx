65031cfd0ad76c83b5bafd600d37e728d24edbdf
Carolina Lunas <carolinalunasfarah@gmail.com>
2025-03-25 18:38:00 -0300

    modify welcome text

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,9 +3,10 @@ const Welcome = () => {
         <section className="flex_col p-4 welcome">
             <h2 className="cursor_default">¡Te damos la bienvenida!</h2>
             <span className="text-center cursor_default">
-                En Finance Track podrás encontrar la evolución anual de{" "}
-                <br></br>
-                acciones de Amazon, Apple, Google, Microsoft y Roku.
+                En Finance Track podrás encontrar la evolución anual de
+                <br />
+                acciones de Amazon, Apple, Coca Cola, Johnson & Johnson, <br />
+                Google, Meta, Microsoft, NVIDIA y Roku.
             </span>
         </section>
     );
